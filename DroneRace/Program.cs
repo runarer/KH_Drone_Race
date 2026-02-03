0b574c03c911e2a245407ca6d54f4818519da344
runarer <95697611+runarer@users.noreply.github.com>
2026-02-03 09:42:01 +0100

    Run ThreadRace

--- DroneRace/Program.cs
+++ DroneRace/Program.cs
@@ -1,2 +1,14 @@
 ﻿
+using DroneRace.Model;
+using DroneRace.Services;
 
+Drone[] drones =
+{
+    new("Big Berta",12,15),
+    new("Tiny Timmy",18,10),
+    // new("Robust Roberta",9,20),
+    // new("Quick Quin",10,18),
+    // new("Two Ton Tony",15,12),
+};
+
+ThreadRace.RaceDronesUsingThreads(drones);
