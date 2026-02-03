3f9e3402b0abd0999cdfaf0072d1a300bcba069b
runarer <95697611+runarer@users.noreply.github.com>
2026-02-03 09:30:50 +0100

    Implements the drone race with threads

---
+++ DroneRace/Services/ThreadRace.cs
@@ -0,0 +1,39 @@
+using DroneRace.Model;
+
+
+namespace DroneRace.Services;
+
+public static class ThreadRace
+{
+    public static void RaceDronesUsingThreads(Drone[] participants)
+    {
+        Thread[] threads = [.. participants.Select(participant => new Thread(() => RunDrone(participant)))];
+
+        Console.WriteLine("Race is starting");
+        foreach (var thread in threads)
+        {
+            thread.Start();
+        }
+
+        foreach (var thread in threads)
+        {
+            thread.Join();
+        }
+        Console.WriteLine("Race is finished");
+    }
+
+    public static void RunDrone(Drone drone)
+    {
+        Console.WriteLine($"Drone {drone.Name} started its run.");
+
+        for (int i = 0; i < drone.MaxCheckpoints; i++)
+        {
+            Thread.Sleep(drone.DelayMs);
+            Console.WriteLine($"Drone reached checkpoint {i + 1}");
+
+        }
+        Console.WriteLine($"Drone {drone.Name} reached its final destination");
+    }
+
+
+}
