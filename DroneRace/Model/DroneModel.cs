36d8829d0c4be9311f9dbfb89a9bb3345feb2a92
runarer <95697611+runarer@users.noreply.github.com>
2026-02-03 09:32:43 +0100

    Simplifies record

--- DroneRace/Model/DroneModel.cs
+++ DroneRace/Model/DroneModel.cs
@@ -1,8 +1,3 @@
 namespace DroneRace.Model;
 
-public record Drone
-{
-    public required string Name { get; set; }
-    public required int MaxCheckpoints { get; set; }
-    public required int DelayMs { get; set; }
-}
+public record Drone(string Name, int MaxCheckpoints, int DelayMs);
