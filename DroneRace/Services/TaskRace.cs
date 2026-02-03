056283938a7fd00103fc8e221f1a9e8820e52e55
runarer <95697611+runarer@users.noreply.github.com>
2026-02-03 10:40:18 +0100

    Implementerer Drone race med TaskCompletionSource

---
+++ DroneRace/Services/TaskRace.cs
@@ -0,0 +1,71 @@
+using DroneRace.Model;
+
+namespace DroneRace.Services;
+
+public static class TaskRace
+{
+    public static Task RaceDronesUsingTasks(Drone[] participants)
+    {
+        Task<string>[] tasks = [.. participants.Select(SetupTscForDrone)];
+
+        var mainTsc = new TaskCompletionSource();
+
+        Task.WhenAll(tasks).ContinueWith(allTasks =>
+        {
+            foreach (var name in allTasks.Result)
+            {
+                Console.WriteLine($"Drone {name} has finished.");
+            }
+            mainTsc.SetResult();
+        });
+        return mainTsc.Task;
+    }
+
+    private static Task<string> SetupTscForDrone(Drone drone)
+    {
+        var tsc = new TaskCompletionSource<string>();
+
+        Task.Run(() =>
+        {
+            try
+            {
+                Console.WriteLine($"Drone {drone.Name} starts");
+                RunDrone(drone, tsc);
+            }
+            catch (Exception ex)
+            {
+                tsc.SetException(ex);
+            }
+        });
+
+        return tsc.Task;
+    }
+
+    private static void RunDrone(Drone drone, TaskCompletionSource<string> tsc)
+    {
+        int currentCheckPoint = 0;
+
+        void Continuation()
+        {
+            if (currentCheckPoint < drone.MaxCheckpoints)
+            {
+                var delayTask = Task.Delay(drone.DelayMs);
+
+                var awaiter = delayTask.GetAwaiter();
+
+                awaiter.OnCompleted(() =>
+                {
+                    Console.WriteLine($"Drone {drone.Name} has reached checkpoint {++currentCheckPoint}");
+                    Continuation();
+                });
+            }
+            else
+            {
+                Console.WriteLine($"Drone {drone.Name} has reached final destination.");
+                tsc.SetResult(drone.Name);
+            }
+
+        }
+        Continuation();
+    }
+}
