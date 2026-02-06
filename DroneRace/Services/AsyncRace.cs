261361150106f5fb224e8cf1f13a1f23b5894f73
runarer <95697611+runarer@users.noreply.github.com>
2026-02-06 12:10:24 +0100

    Async race implemented

---
+++ DroneRace/Services/AsyncRace.cs
@@ -0,0 +1,32 @@
+using DroneRace.Model;
+
+namespace DroneRace.Services;
+
+public class AsyncRace
+{
+
+    public static async Task RaceDronesUsingAsync(Drone[] drones)
+    {
+        Task<string>[] tasks = [.. drones.Select(RunDrone)];
+
+        var droneNames = await Task.WhenAll(tasks);
+        foreach (var name in droneNames)
+            Console.WriteLine($"Drone {name} finished");
+    }
+
+    private static async Task<string> RunDrone(Drone drone)
+    {
+        Console.WriteLine($"Drone {drone.Name} starts");
+
+        for (int i = 0; i < drone.MaxCheckpoints; i++)
+        {
+            if (drone.Name == "Tiny Timmy" && i == 5)
+                throw new Exception("Tiny Timmy crashed");
+            await Task.Delay(drone.DelayMs);
+            Console.WriteLine($"Drone {drone.Name} has reached checkpoint {i + 1}");
+        }
+        Console.WriteLine($"Drone {drone.Name} has completed");
+        return drone.Name;
+    }
+
+}
