96003e36ec8b49153c2342fb6faa8e0b497f8f52
runarer <95697611+runarer@users.noreply.github.com>
2026-02-06 12:40:34 +0100

    Basic frame of listener

---
+++ ControlTowerAPI/Listener/ControlTower.cs
@@ -0,0 +1,64 @@
+
+
+using System.Net;
+using ControlTowerAPI.Model;
+
+namespace ControlTowerAPI.Listener;
+
+public class ControlTower
+{
+    private HttpListener _listener;
+    private Dictionary<string, Drone> _registredDrones;
+
+    public ControlTower(string uriPrefix)
+    {
+        _listener = new();
+
+        _listener.Prefixes.Add(uriPrefix);
+    }
+
+    public async Task StartListener()
+    {
+        _listener.Start();
+
+        while (true)
+        {
+            try
+            {
+                var context = await _listener.GetContextAsync();
+                if (context.Request.HttpMethod != "GET")
+                    await ProcessBadRequest(context);
+                else
+                    await ProcessGetRequest(context);
+            }
+            catch (HttpListenerException) { break; }
+            catch (ArgumentException) { break; }
+        }
+    }
+
+    private async Task ProcessBadRequest(HttpListenerContext context)
+    {
+
+    }
+
+    private async Task ProcessGetRequest(HttpListenerContext context)
+    {
+
+    }
+
+    private async Task RegisterDrone(HttpListenerContext context)
+    {
+
+    }
+
+    private async Task GetWeather(HttpListenerContext context)
+    {
+
+    }
+
+    private async Task<Drone?> GetRoute(string name)
+    {
+
+    }
+
+}
