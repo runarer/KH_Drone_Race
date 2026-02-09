bc07e832b5d5b4c53e53a269b91aeb476628b4c4
runarer <95697611+runarer@users.noreply.github.com>
2026-02-09 14:43:45 +0100

    Change stop message

--- ControlTowerAPI/Program.cs
+++ ControlTowerAPI/Program.cs
@@ -6,7 +6,7 @@ var tower = new ControlTower("http://localhost:6060/");
 try
 {
     _ = tower.StartListener();
-    Console.WriteLine("Press any key to stop burn down the tower...");
+    Console.WriteLine("Press any key to stop the tower...");
     Console.ReadKey(intercept: true);
 }
 finally
