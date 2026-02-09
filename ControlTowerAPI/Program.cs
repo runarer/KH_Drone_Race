133b228c67233bacf48632e149f70829761631e0
runarer <95697611+runarer@users.noreply.github.com>
2026-02-09 12:02:21 +0100

    Main app code, to run the service

--- ControlTowerAPI/Program.cs
+++ ControlTowerAPI/Program.cs
@@ -1,2 +1,15 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿
+using ControlTowerAPI.Listener;
+
+var tower = new ControlTower("http://localhost:6060");
+
+try
+{
+    _ = tower.StartListener();
+    Console.WriteLine("Press any key to stop burn down the tower...");
+    Console.ReadKey(intercept: true);
+}
+finally
+{
+    tower.Stop();
+}
