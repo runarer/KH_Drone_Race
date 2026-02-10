e532c9c72ae0cfb3d5441b74b6d9c10ccbdd64d0
runarer <95697611+runarer@users.noreply.github.com>
2026-02-10 09:52:25 +0100

    Run everyting after each other

--- DroneRace/Program.cs
+++ DroneRace/Program.cs
@@ -3,17 +3,21 @@ using DroneRace.Model;
 using DroneRace.Services;
 
 Drone[] drones =
-{
+[
     new("Big Berta",12,15),
-    new("Tiny Timmy",18,10),
-    // new("Robust Roberta",9,20),
-    // new("Quick Quin",10,18),
-    // new("Two Ton Tony",15,12),
-};
+        new("Tiny Timmy",18,10),
+        // new("Robust Roberta",9,20),
+        // new("Quick Quin",10,18),
+        // new("Two Ton Tony",15,12),
+    ];
+
 
 ThreadRace.RaceDronesUsingThreads(drones);
 
 TaskRace.RaceDronesUsingTasks(drones).Wait();
+
+await AsyncRace.RaceDronesUsingAsync(drones);
+
 
 try
 {
@@ -23,6 +27,7 @@ catch (Exception ex)
 {
     Console.WriteLine(ex.Message);
 }
+
 
 try
 {
