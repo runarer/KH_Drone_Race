e5be8959420b42dba79237a9dc52ece6ea504f13
runarer <95697611+runarer@users.noreply.github.com>
2026-02-10 14:56:21 +0100

    Remove some commented out code

--- DroneRaceApp/Program.cs
+++ DroneRaceApp/Program.cs
@@ -14,9 +14,6 @@ class Program
     [
         new("Big Berta",12,15),
         new("Tiny Timmy",18,10),
-        // new("Robust Roberta",9,20),
-        // new("Quick Quin",10,18),
-        // new("Two Ton Tony",15,12),
     ];
     static async Task Main()
     {
