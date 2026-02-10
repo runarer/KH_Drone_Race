c89376da3fa8c9ddf027354df627e02517301589
runarer <95697611+runarer@users.noreply.github.com>
2026-02-10 10:41:36 +0100

    Common model in its own project

---
+++ DroneModel/DroneModel.cs
@@ -0,0 +1,9 @@
+
+namespace DroneModel;
+
+public class Drone
+{
+    public required string Name { get; set; }
+    public required int MaxCheckpoints { get; set; }
+    public required int DelayMs { get; set; }
+};
